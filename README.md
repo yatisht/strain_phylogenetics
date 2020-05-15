639750dfba3cf557f747436af3f64b7b264971b5
Yatish Turakhia <yatisht@stanford.edu>
2020-05-15 11:56:00 -0700

    Full output.

--- README.md
+++ README.md
@@ -4,8 +4,16 @@
 ```
     $ pip install treelib
 ```
-* Run example
+* Download example newick files
 ```
-    $ wget -rkpN -e robots=off -np -A nh,json https://hgwdev.gi.ucsc.edu/~angie/nextstrainRecompute.2020-04-30/
-    $ python compute_directed_change.py -T1 T1.nh -T2 T2.nh
+    $ for day in 19 20 21 22 23 24 25 26; do wget https://hgwdev.gi.ucsc.edu/~angie/nextstrainRecompute.2020-04-30/2020-04-${day}/nextstrain.nh -O tree/nextstrain-2020-04-${day}.nh; done
+```
+* Download example VCF files
+```
+    $ for day in 19 20 21 22 23 24 25 26; do wget https://hgwdev.gi.ucsc.edu/~angie/nextstrainRecompute.2020-04-30/2020-04-${day}/nextstrainSamples.vcf.gz -O vcf/nextstrain-2020-04-${day}.vcf.gz; done
+```
+* Run examples
+```
+    $ python compute_directed_change-cog.py -T1 tree/nextstrain-2020-04-19.nh -T2 tree/nextstrain-2020-04-20.nh -CORES=2
+    $ python find_parsimonious_assignments.py -tree tree/nextstrain-2020-04-19.nh -vcf vcf/nextstrain-2020-04-19.vcf.gz 
 ```
--- compute_directed_change.py
+++ compute_directed_change.py
@@ -4,30 +4,18 @@ import argparse
 from multiprocessing import Pool
 import multiprocessing
 import functools
-import matplotlib.pyplot as plt
+from scipy.stats import entropy
+import random
 
 def create_tree(tree_filename):
     tree = Tree()
+    # Read Newick file line
     f = open(tree_filename)
-    lines = f.readlines()
-    first_line = lines[0]
-    l = first_line.rstrip()
-    s1 = l.split(',')
-    s2 = [s.split(':')[0].replace('(', '').replace(')', '') for s in s1]
-    s3 = l.split('#')
-    s4 = [s.split(')')[1] for s in s3 if (len(s.split(')')) > 1)]
-    s5 = []
-    start = False
-    word = ''
-    for c in l:
-        if c == '#':
-            start = True
-        elif ((start) and (c != ':')):
-            word += c
-        elif ((start) and (c == ':')):
-            start = False
-            s5.append(word)
-            word = ''
+    line = f.readline().rstrip()
+    f.close()
+    # Get leaves names in s2
+    s1 = line.split(',')
+    s2 = [s.split(':')[0].replace('(', '').split(')')[0] for s in s1]
     stack = [(w.count('('), w.count(')')) for w in s1]
     num_open = sum([s[0] for s in stack])
     num_close = sum([s[1] for s in stack])
@@ -40,6 +28,7 @@ def create_tree(tree_filename):
     curr_idx = 0
     parent_stack = []
 
+    # Construct the tree in treelib format
     for (k, species) in enumerate(s2):
         no = stack[k][0]
         nc = stack[k][1]
@@ -53,10 +42,95 @@ def create_tree(tree_filename):
         tree.create_node(species, species, parent=parent_stack[-1])
         for i in range(nc):
             nid = parent_stack[-1]
-            tree.update_node(nid, tag=(nid+':'+s4[curr_idx]+':'+s5[curr_idx]))
             curr_idx += 1
             parent_stack.pop()
 
+    return tree
+
+# Convert tree back to Newick string
+def get_newick_string (tree):
+    dfs = [n for n in tree.expand_tree(mode=1, sorting=False, reverse=True)]
+    depths = [tree.level(n) for n in dfs]
+    is_leaf = [tree.get_node(n).is_leaf() for n in dfs]
+    curr_depth = -1
+    newick_string = ''
+    prev_open = True 
+    stack = []
+    for (k,s) in enumerate(dfs):
+        tag = tree.get_node(s).tag
+        depth = depths[k]
+        if (curr_depth < depth):
+            if (not prev_open):
+                newick_string += ','
+            for i in range(depth-max(0,curr_depth)):
+                newick_string += '('
+                prev_open = True
+            if (is_leaf[k]):
+                newick_string += tag
+                prev_open = False
+            else:
+                stack.append(tag)
+        elif (curr_depth > depth):
+            prev_open = False
+            for i in range(curr_depth-depth):
+                newick_string += ')'
+                newick_string += stack[-1]
+                stack.pop()
+            if (is_leaf[k]):
+                newick_string += ','+tag
+            else:
+                stack.append(tag)
+        else:
+            prev_open = False
+            if (is_leaf[k]):
+                newick_string += ','+tag
+            else:
+                stack.append(tag)
+        curr_depth = depth
+    for i in range(curr_depth):
+        newick_string += ')'
+        newick_string += stack[-1]
+        stack.pop()
+    newick_string += ';'
+    return newick_string
+
+def create_tree(tree_filename):
+    tree = Tree()
+    clade_roots = {}
+    f = open(tree_filename)
+    lines = f.readlines()
+    first_line = lines[0]
+    l = first_line.rstrip()
+    s1 = l.split(',')
+    s2 = [s.split(':')[0].replace('(', '').replace(')', '') for s in s1]
+    s3 = [s.split('|')[1] if (len(s.split('|')) > 1) else s for s in s2]
+    s4 = [s.replace("'", "") for s in s3]
+    stack = [(w.count('('), w.count(')')) for w in s1]
+    num_open = sum([s[0] for s in stack])
+    num_close = sum([s[1] for s in stack])
+
+    if ((num_open != num_close)):
+        print ('ERROR: PhyloTree in incorrect format!')
+        sys.exit()
+
+    curr_node = '0'
+    curr_idx = 0
+    parent_stack = []
+
+    for (k, species) in enumerate(s4):
+        no = stack[k][0]
+        nc = stack[k][1]
+        for i in range(no):
+            curr_node = str(int(curr_node)+1)
+            if len(parent_stack) == 0:
+                tree.create_node(curr_node, curr_node)
+            else:
+                tree.create_node(curr_node, curr_node, parent=parent_stack[-1])
+            parent_stack.append(curr_node)
+        tree.create_node(species, species, parent=parent_stack[-1])
+        for i in range(nc):
+            curr_idx += 1
+            parent_stack.pop()
     return tree
 
 def get_all_node_ids (tree):
@@ -71,7 +145,7 @@ def get_leaf_node_ids (tree):
     leaf_ids = [n.identifier for n in tree.leaves()]
     return leaf_ids
 
-def get_branch_dichotomy (tree, bid):
+def get_branch_split (tree, bid):
     all_leaf_ids = set([n.identifier for n in tree.leaves()])
     A = set([n.identifier for n in tree.leaves(bid)])
     B = (all_leaf_ids - A)
@@ -82,10 +156,47 @@ def get_symmetric_difference (A, X, common_leaf_ids):
     d2 = len((set(X)-set(A)).intersection(common_leaf_ids)) 
     return (d1+d2)
 
-def get_directed_change(bid, tree1, tree2_dichotomies, common_leaf_ids):
-    (A, B, bid) = get_branch_dichotomy(tree1, bid)
-    return (min([(get_symmetric_difference(A,X,common_leaf_ids), bid2) \
-                 for (X,Y,bid2) in tree2_dichotomies]))
+def get_split_distance(bid, tree1, tree2_splits, common_leaf_ids):
+    (A, B, bid) = get_branch_split(tree1, bid)
+    dist = [(get_symmetric_difference(A,X,common_leaf_ids), bid2) \
+               for (X,Y,bid2) in tree2_splits] 
+    min_dist = min(dist)[0]
+    ret = [(0, '-1')]
+    if (min_dist != len(tree1.leaves(bid))):
+        ret = [x for x in dist if (x[0] == min_dist)]
+    return ret
+
+def get_entropy_weighted_split_distance(bid, tree1, tree2, tree2_splits, \
+                                         common_leaf_ids):
+    if (len(common_leaf_ids) == 0):
+        return [(0, 0, '-1')]
+    (A, B, bid) = get_branch_split(tree1, bid)
+    s = len(set(A).intersection(common_leaf_ids))
+    p = (1.0*s)/len(common_leaf_ids)
+    q = 1.0 - p
+    ent = entropy([p, q], base=2)
+    dist = []
+    min_dist = 1e9
+    if (p == 0):
+        return [(0, 0, '-1')]
+    for (X,Y,bid2) in tree2_splits:
+        diff = get_symmetric_difference(A,X,common_leaf_ids)
+        dist.append((diff, ent*diff, bid2))
+        if (diff < min_dist):
+            min_dist = diff
+    if min_dist > s: 
+        return [(s, ent*s, '-1')]
+    m = [x for x in dist if (x[0] == min_dist)]
+    ret = []
+    for i in range(len(m)):
+        found_anc = False
+        for j in range(len(m)):
+            if (i != j) and (tree2.is_ancestor(m[j][2], m[i][2])):
+                found_anc = True
+                break
+        if not found_anc:
+            ret.append(m[i])
+    return ret
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Compute directed change '
@@ -94,22 +205,38 @@ if __name__ == "__main__":
                         help="tree 1 (in Newick format)")
     parser.add_argument("-T2", type=str,
                         help="tree 2 (in Newick format)")
+    parser.add_argument("-CORES", type=str,
+                        help="number of CPU cores (default=1) to use [OPTIONAL]")
 
     if len(sys.argv) <= 4:
         parser.print_help()
         sys.exit(1)
 
     args = vars(parser.parse_args())
-    T1_filename = args['T1']
-    T2_filename = args['T2']
+    T1_filename = args.get('T1', '')
+    if (T1_filename == None):
+        parser.print_help()
+        sys.exit(1)
+    T2_filename = args.get('T2', '')
+    if (T2_filename == None):
+        parser.print_help()
+        sys.exit(1)
+    num_cores = args.get('CORES', '')
+    if (num_cores == None):
+        num_cores = '1'
+    
     T1 = create_tree(T1_filename)
     T2 = create_tree(T2_filename)
 
-    print ('T1: ')
-    T1.show()
+    T1_newick = get_newick_string(T1)
+    T2_newick = get_newick_string(T2)
+    
+    print 'T1 (in newick format) with internal nodes labelled: '
+    print T1_newick 
+    
+    print 'T2 (in newick format) with internal nodes labelled: '
+    print T2_newick 
 
-    print ('T2: ')
-    T2.show()
 
     T1_internal_node_ids = get_internal_node_ids(T1)
     T2_internal_node_ids = get_internal_node_ids(T2)
@@ -118,49 +245,32 @@ if __name__ == "__main__":
     T2_leaf_node_ids = get_leaf_node_ids(T2)
     common_leaf_ids = set(T1_leaf_node_ids).intersection(set(T2_leaf_node_ids))
     
-    T2_dichotomies = [get_branch_dichotomy(T2, bid) for bid in \
+    T2_splits = [get_branch_split(T2, bid) for bid in \
                          T2_internal_node_ids] 
 
-    pool = Pool(processes=multiprocessing.cpu_count())
+    pool = Pool(processes=int(num_cores))
     
-    worker = functools.partial(get_directed_change, tree1=T1, \
-        tree2_dichotomies=T2_dichotomies, common_leaf_ids = common_leaf_ids) 
+    worker = functools.partial(get_entropy_weighted_split_distance, tree1=T1, \
+                               tree2=T2, tree2_splits=T2_splits, \
+                               common_leaf_ids = common_leaf_ids) 
 
     # directed change for each branch in T1
     C = pool.map(worker, T1_internal_node_ids)
 
+    total_dist = 0.0
+    print '#T1_node\tT2_best_matches\tmatching_split_distance'+\
+            '\tentropy_weighted_split_distance'
     for (k, bid) in enumerate(T1_internal_node_ids):
         tag1 = T1.get_node(bid).tag
-        tag2 = T2.get_node(C[k][1]).tag 
-        T1.update_node(bid, tag=(tag1+" change="+str(C[k][0])+\
-                                        " best-match="+tag2))
+        T2_tags_arr = [T2.get_node(m[2]).tag if m[2] != '-1' else '-1' \
+                       for m in C[k]] 
+        T2_tags = ','.join(T2_tags_arr)
+        print tag1+'\t'+T2_tags+'\t'+str(C[k][0][0])+'\t'+str(C[k][0][1])
+        total_dist += C[k][0][1]
 
-    print ('T1 (with directed changes): ')
-    T1.show()
+    print 'T1 size: ', len(T1_leaf_node_ids)
+    print 'T2 size: ', len(T2_leaf_node_ids)
+    print 'Total common leaves: ', len(list(common_leaf_ids))
+    print 'Total entropy-weighted split distance: ', total_dist
 
-    print ('T1 size: '+str(len(T1_internal_node_ids)))
-    print ('T2 size: '+str(len(T2_internal_node_ids)))
-
-    clade_changes = {}
-
-    for lid in common_leaf_ids:
-        if T1.level(lid) != 0:
-            parent_tag = T1.parent(lid).tag
-            T1_clade = parent_tag.split()[0].split(':')[1]
-            T2_clade = parent_tag.split()[2].split('=')[1].split(':')[1]
-            if T1_clade != T2_clade:
-                if (T1_clade != 'unassigned') and (T2_clade != 'unassigned'):
-                    key = T1_clade + "<-" + T2_clade
-                elif (T1_clade == 'unassigned'):
-                    key =  "unassigned<-*" 
-                else:
-                    key =  "*<-unassigned" 
-                n = clade_changes.get(key, 0)
-                clade_changes[key] = n+1
-
-    for (k,v) in clade_changes.items():
-        print (k+": "+str(v))
-
-    print (len(C))
-
-
+    
