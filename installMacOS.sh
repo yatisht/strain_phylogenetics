607233aec8f1ec7d92c7c2fc341d85185b8678b7
Yatish Turakhia <yatisht@stanford.edu>
2020-09-07 16:33:02 -0700

    Update installMacOS.sh

--- installMacOS.sh
+++ installMacOS.sh
@@ -5,6 +5,6 @@ mkdir build
 cd build
 ls ${PWD}/../tbb
 ls ${PWD}/../tbb/cmake
-cmake -DTBB_DIR=${PWD}/../tbb/cmake -DTBB_ROOT=${PWD}/../tbb -DOpenMP_CXX_FLAGS="-Xclang -fopenmp -I/usr/local/opt/libomp/include" -DOpenMP_C_FLAGS="-Xclang -fopenmp -I/usr/local/opt/libomp/include" -DOpenMP_CXX_LIB_NAMES=libomp  -DOpenMP_C_LIB_NAMES=libomp  -DOpenMP_libomp_LIBRARY=/usr/local/opt/libomp/lib/libomp.dylib -DCMAKE_SHARED_LINKER_FLAGS="-L/usr/local/opt/libomp/lib -lomp -Wl,-rpath,/usr/local/opt/libomp/lib" ..
+cmake -DTBB_DIR=${PWD}/../tbb -DTBB_ROOT=${PWD}/../tbb -DOpenMP_CXX_FLAGS="-Xclang -fopenmp -I/usr/local/opt/libomp/include" -DOpenMP_C_FLAGS="-Xclang -fopenmp -I/usr/local/opt/libomp/include" -DOpenMP_CXX_LIB_NAMES=libomp  -DOpenMP_C_LIB_NAMES=libomp  -DOpenMP_libomp_LIBRARY=/usr/local/opt/libomp/lib/libomp.dylib -DCMAKE_SHARED_LINKER_FLAGS="-L/usr/local/opt/libomp/lib -lomp -Wl,-rpath,/usr/local/opt/libomp/lib" ..
 make -j
 cd ..
