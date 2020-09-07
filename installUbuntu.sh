de35bd3b48ed2a8cdd75b34aeb31f0e36c4649cb
Yatish Turakhia <yatisht@stanford.edu>
2020-09-07 16:34:13 -0700

    Update installUbuntu.sh

--- installUbuntu.sh
+++ installUbuntu.sh
@@ -8,7 +8,7 @@ sudo apt-get --yes install libprotoc-dev libprotoc-dev protobuf-compiler
 git clone https://github.com/01org/tbb
 mkdir build
 cd build
-cmake  -DTBB_ROOT=${PWD}/../tbb  .. 
+cmake  -DTBB_DIR=${PWD}/../tbb  -DTBB_ROOT=${PWD}/../tbb .. 
 make -j
 cd ..
     
