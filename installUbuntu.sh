19e010fa989a5d854decc49f7e3f5c44f31413e2
Yatish Turakhia <yatisht@stanford.edu>
2020-09-07 16:07:37 -0700

    Update installUbuntu.sh

--- installUbuntu.sh
+++ installUbuntu.sh
@@ -1,5 +1,9 @@
 sudo apt update 
-sudo apt-get --yes install build-essential cmake libboost-all-dev libomp-dev libprotoc-dev libprotoc-dev protobuf-compiler
+sudo apt-get --yes install build-essential
+sudo apt-get --yes install cmake
+sudo apt-get --yes install libboost-all-dev
+sudo apt-get --yes install libomp-dev
+sudo apt-get --yes install libprotoc-dev libprotoc-dev protobuf-compiler
 
 git clone https://github.com/01org/tbb
 mkdir build
