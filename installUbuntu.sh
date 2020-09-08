80df9aa6171cda2fa7f7957f200fc69c2457f684
Yatish Turakhia <yatisht@stanford.edu>
2020-09-07 17:24:40 -0700

    Use specific release of tbb for Ubuntu.

--- installUbuntu.sh
+++ installUbuntu.sh
@@ -5,10 +5,11 @@ sudo apt-get --yes install libboost-all-dev
 sudo apt-get --yes install libomp-dev
 sudo apt-get --yes install libprotoc-dev libprotoc-dev protobuf-compiler
 
-git clone https://github.com/01org/tbb
+wget https://github.com/oneapi-src/oneTBB/releases/download/2019_U9/tbb2019_20191006oss_lin.tgz
+tar -xvzf tbb2019_20191006oss_lin.tgz
 mkdir build
 cd build
-cmake  -DTBB_DIR=${PWD}/../tbb  -DTBB_ROOT=${PWD}/../tbb .. 
+cmake  -DTBB_DIR=${PWD}/../tbb2019_20191006oss  -DTBB_ROOT=${PWD}/../tbb2019_20191006oss .. 
 make -j
 cd ..
     
