80e6120bd8ef11ae9b6c08ea7b4a395855dd35d8
Yatish Turakhia <yatisht@stanford.edu>
2020-09-07 17:20:10 -0700

    Use specific tbb release for macOS.

--- installMacOS.sh
+++ installMacOS.sh
@@ -1,10 +1,9 @@
 brew install cmake boost libomp protobuf
 
-git clone https://github.com/01org/tbb
+wget https://github.com/oneapi-src/oneTBB/releases/download/2019_U9/tbb2019_20191006oss_mac.tgz
+tar -xvzf tbb2019_20191006oss_mac.tgz
 mkdir build
 cd build
-ls ${PWD}/../tbb
-ls ${PWD}/../tbb/cmake
-cmake -DTBB_DIR=${PWD}/../tbb -DOpenMP_CXX_FLAGS="-Xclang -fopenmp -I/usr/local/opt/libomp/include" -DOpenMP_C_FLAGS="-Xclang -fopenmp -I/usr/local/opt/libomp/include" -DOpenMP_CXX_LIB_NAMES=libomp  -DOpenMP_C_LIB_NAMES=libomp  -DOpenMP_libomp_LIBRARY=/usr/local/opt/libomp/lib/libomp.dylib -DCMAKE_SHARED_LINKER_FLAGS="-L/usr/local/opt/libomp/lib -lomp -Wl,-rpath,/usr/local/opt/libomp/lib" ..
+cmake -DTBB_DIR=${PWD}/../tbb2019_20191006oss -DCMAKE_PREFIX_PATH=${PWD}/../tbb2019_20191006oss/cmake -DOpenMP_CXX_FLAGS="-Xclang -fopenmp -I/usr/local/opt/libomp/include" -DOpenMP_C_FLAGS="-Xclang -fopenmp -I/usr/local/opt/libomp/include" -DOpenMP_CXX_LIB_NAMES=libomp  -DOpenMP_C_LIB_NAMES=libomp  -DOpenMP_libomp_LIBRARY=/usr/local/opt/libomp/lib/libomp.dylib -DCMAKE_SHARED_LINKER_FLAGS="-L/usr/local/opt/libomp/lib -lomp -Wl,-rpath,/usr/local/opt/libomp/lib" ..
 make -j
 cd ..
