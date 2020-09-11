997b2bb8af9aaf2395ac2f2181bc5a1c1b38d112
Yatish Turakhia <yatisht@stanford.edu>
2020-09-11 11:12:35 -0700

    Update installMacOS.sh

--- installMacOS.sh
+++ installMacOS.sh
@@ -1,6 +1,6 @@
 brew install cmake boost libomp protobuf wget python@2
 
-pip2 install treelib numpy==1.14.6 scipy==1.0.1
+pip2 install treelib numpy scipy
 
 wget https://github.com/oneapi-src/oneTBB/releases/download/2019_U9/tbb2019_20191006oss_mac.tgz
 tar -xvzf tbb2019_20191006oss_mac.tgz
